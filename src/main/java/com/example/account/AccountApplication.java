fd1a6875b6d10cf3664ff314a0c249d73e1be46d
escha-k <bbs7380@gmail.com>
2025-10-15 22:41:31 +0900

    ci 테스트를 위한 코드 변경

--- src/main/java/com/example/account/AccountApplication.java
+++ src/main/java/com/example/account/AccountApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccountApplication {
 
 	public static void main(String[] args) {
+		System.out.println("ci 테스트를 위한 코드 추가");
+
 		SpringApplication.run(AccountApplication.class, args);
 	}
 
